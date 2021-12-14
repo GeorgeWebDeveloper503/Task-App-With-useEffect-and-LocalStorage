7b1a4abf02e758a96cdbbe3c45d1f54df9b5ccdf
GeorgeWebDeveloper503 <georgedeveloper951@gmail.com>
2021-12-14 00:41:48 -0600

    Main Table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [userName, setUserName] = useState('George');
+  const [taskItems, settaskItems] = useState([
+      {name:"Task One", done: false},
+      {name:"Task Two", done: false},
+      {name:"Task Three", done: true},
+      {name:"Task Four", done: false},
+    ]);
+
+    const taskTableRows = () =>{
+      return taskItems.map(task =>(
+        <tr key={task.name}>
+          <td>{task.name}</td>
+          <td>{String(task.done)}</td>
+        </tr>
+      ))
+    };
+
+
   return (
     <div>
-     <h1>Hello world</h1>
+    
+        <table className="table striped bordered hover">
+          <thead>
+            <tr>
+              <th scope="col">Description</th>
+              <th scope="col">Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            {taskTableRows()}
+          </tbody>
+        </table>
     </div>
   );
 }
